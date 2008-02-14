8c63ba771262e5381f453eb61d59c1f80b84f0f0
Aaron Parecki <aaron@parecki.com>
2008-02-14 17:30:06 +0000

    added script which maintains the mailing list users
    git-svn-id: file:///Users/aaronpk/Sites/pathways-svn/trunk@242 e3ad4a73-d236-0410-80c3-982009a79c7a

---
+++ www/oregon-pathways-users-emails.php
@@ -0,0 +1,35 @@
+<?php
+include("inc.php");
+
+
+if( $_SERVER['REMOTE_ADDR'] != '207.210.65.232' ) {
+	die();
+}
+
+
+header("Content-type: text/plain");
+
+$schools = $DB->MultiQuery("
+	SELECT schools.*
+	FROM schools
+	INNER JOIN users ON users.school_id=schools.id
+	GROUP BY schools.id
+	ORDER BY school_name
+	");
+
+
+foreach( $schools as $s ) {
+
+	$users = $DB->MultiQuery("
+		SELECT users.id, first_name, last_name, email, lev.name AS user_level, last_logon, last_logon_ip
+		FROM users, admin_user_levels AS lev
+		WHERE school_id=".$s['id']."
+			AND lev.level = users.user_level
+		");
+	foreach( $users as $u ) {
+		echo $u['email']."\n";
+	}
+
+}
+
+?>
