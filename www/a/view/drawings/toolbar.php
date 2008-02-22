977dd0fda17fd67b607b5936275acd58646b78a0
Ryan Berdeen <ryan@lunarlogic.com>
2008-02-22 21:47:09 +0000

    fixed link to print page to use new URL format
    git-svn-id: file:///Users/aaronpk/Sites/pathways-svn/trunk@258 e3ad4a73-d236-0410-80c3-982009a79c7a

--- www/a/view/drawings/toolbar.php
+++ www/a/view/drawings/toolbar.php
@@ -9,6 +9,6 @@ $drawing_main = $DB->SingleQuery("SELECT * FROM drawing_main WHERE id=".$drawing
 	<div id="toolbar_content">
 		<?php if ($publishAllowed) : ?><a href="<?= $_SERVER['PHP_SELF'].'?action=version_info&amp;version_id='.$_REQUEST['drawing_id'] ?>" class="toolbarButton">publish this version</a><?php endif; ?>
 		<a href="copy_popup.php?version_id=<?=  $_REQUEST['drawing_id'] ?>" class="toolbarButton" onclick="return showCopy(this);">copy this version</a>
-		<a href="/c/version/<?= $_REQUEST['version_id'] ?>.html?action=print" class="toolbarButton">print this version</a>
+		<a href="/c/version/<?= $drawing_main['code'] . '/' . $drawing['version_num'] ?>.html?action=print" class="toolbarButton">print this version</a>
 	</div>
 </div>
