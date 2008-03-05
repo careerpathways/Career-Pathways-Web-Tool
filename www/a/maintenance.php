78e3e1fc565720d021fc96dfbdf841dd5baf6be0
Aaron Parecki <aaron@parecki.com>
2008-03-05 19:14:08 +0000

    added maintenance page so we can use it again later
    git-svn-id: file:///Users/aaronpk/Sites/pathways-svn/trunk@282 e3ad4a73-d236-0410-80c3-982009a79c7a

---
+++ www/a/maintenance.php
@@ -0,0 +1,12 @@
+<?php
+chdir("..");
+include("inc.php");
+
+PrintHeader();
+?>
+
+<p>This website is currently undergoing routine maintenance. You will be unable to log in to edit your drawings until March 5, 12:00pm</p>
+
+<?php
+PrintFooter();
+?>
