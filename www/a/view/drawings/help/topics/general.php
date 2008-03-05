201b5b9871ac78578a3b154845fd7eb1d1f6e79b
Ryan Berdeen <ryan@lunarlogic.com>
2008-03-05 06:28:06 +0000

    #68
    help text changes
    
    git-svn-id: file:///Users/aaronpk/Sites/pathways-svn/trunk@276 e3ad4a73-d236-0410-80c3-982009a79c7a

--- www/a/view/drawings/help/topics/general.php
+++ www/a/view/drawings/help/topics/general.php
@@ -1,11 +1,26 @@
 <h2>General</h2>
 
-<p>Right-click the chart to edit the grid or create a new object.</p>
+<p>Right-click the drawing area tools menu to show and/or edit the grid, and/or create new objects.</p>
 
 <p>Right-click an object for a menu of customizing options.</p>
 
-<p>Change an object's color by selecting it and clicking a color or using <var>Color</var> on the menu.</p>
+<p>Change an object's color by selecting it and clicking a color from <var>TOOLS</var>, or using <var>Color</var> on the menu.</p>
 
 <p>Delete the selected object with the <kbd>DELETE</kbd> key or the object's menu.</p>
 
-<p>Duplicate the selected object with <kbd>CTRL+C</kbd>, <kbd>CTRL+V</kbd>, or the object's menu.</p>
+<p>Duplicate the selected object with <kbd>CTRL+C</kbd>, <kbd>CTRL+V</kbd>, or the object's menu. Offsets new duplicate below original.</p>
+
+
+<h2>Positioning Connections, Lines and Arrows</h2>
+
+<p>When selected, Connections, Lines and Arrows now show a blue box frame, with a Green start point and Red end point. The object’s menu can be brought up anywhere within this box. Click and drag the Green start point or Red end point to adjust the starting or ending location.</p>
+
+
+<p>Resizing Boxes</p>
+
+<p>When selected, boxes are highlighted with a blue box frame, with LEFT and RIGHT blue adjustment points. Click and drag either point to expand/retract box width. Box height is determined by content.</p>
+
+
+<p>Print This Version</p>
+
+<p>Click print this version from <var>TOOLS</var> to render a drawing properly for printing from the browser menu.</p>
--- www/a/view/drawings/help/topics/simple_widget_menu.php
+++ www/a/view/drawings/help/topics/simple_widget_menu.php
@@ -5,7 +5,7 @@
 <dd>Sets the color of the line or arrow.</dd>
 
 <dt><var>Duplicate</var></dt>
-<dd>Makes a copy of the line or arrow.</dd>
+<dd>Makes a copy of the line or arrow. Offsets new duplicate below original.</dd>
 
 <dt><var>Delete</var></dt>
 <dd>Permanently removes the line or arrow, after confirmation.</dd>
