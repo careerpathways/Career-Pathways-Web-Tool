177fc74201c544dfbd16ce335d165f9af98a1108
Torgie Madison <torgie@gmail.com>
2009-01-20 05:46:59 +0000

    Makes HTML Preview of POST Drawings.
    git-svn-id: file:///Users/aaronpk/Sites/pathways-svn/trunk@365 e3ad4a73-d236-0410-80c3-982009a79c7a

--- www/c/post_view.php
+++ www/c/post_view.php
@@ -7,7 +7,7 @@ $drawing_id = 0;
 
 if( Request('page') == 'published' ) {
 	$drawing = $DB->SingleQuery('SELECT main.*, schools.school_abbr, d.id
-		FROM ccti_drawing_main AS main, ccti_drawings AS d, schools 
+		FROM post_drawing_main AS main, ccti_drawings AS d, schools 
 		WHERE d.parent_id = main.id
 			AND main.school_id = schools.id
 			AND published = 1
@@ -19,7 +19,7 @@ if( Request('page') == 'published' ) {
 } elseif( Request('page') == 'version') {
 
 	$drawing = $DB->SingleQuery('SELECT main.*, schools.school_abbr, d.id
-		FROM ccti_drawing_main AS main, ccti_drawings AS d, schools
+		FROM post_drawing_main AS main, ccti_drawings AS d, schools
 		WHERE d.parent_id = main.id
 			AND main.school_id = schools.id
 			AND version_num = '.Request('v').'
@@ -36,14 +36,20 @@ if( $drawing_id == 0 ) {
 
 
 $page_title = 'test';
-
-
-$TEMPLATE->addl_styles[] = "/c/pstyle.css";
-
-echo '<div id="post_title"><img src="/files/titles/'.base64_encode($drawing['school_abbr']).'/'.base64_encode($page_title).'.png" /></div>';
-
-$post = POSTChart::Create($drawing_id);
-$post->display();
-
-
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+	<title><?=$page_title?></title>
+	<link rel="stylesheet" href="/c/pstyle.css" />
+</head>
+<body>
+	<div id="post_title">
+		<img src="/files/titles/<?=base64_encode($drawing['school_abbr'])?>/<?=base64_encode($page_title)?>.png" alt="Career POST" />
+	</div>
+<?php
+	$post = POSTChart::Create($drawing_id);
+	$post->display();
+?>
+</body>
+</html>
