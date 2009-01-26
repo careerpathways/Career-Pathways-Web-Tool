c05fa72399e188f634f3193f9a11bf8e848a6491
Aaron Parecki <aaron@parecki.com>
2009-01-26 01:52:12 +0000

    added a page to look at the page permissions
    git-svn-id: file:///Users/aaronpk/Sites/pathways-svn/trunk@393 e3ad4a73-d236-0410-80c3-982009a79c7a

---
+++ www/a/permissions.php
@@ -0,0 +1,40 @@
+<?php
+chdir("..");
+include("inc.php");
+
+ModuleInit('permissions');
+
+
+PrintHeader();
+
+$levels = $DB->MultiQuery('SELECT * FROM admin_user_levels ORDER BY level DESC');
+
+echo '<table class="bordered">';
+echo '<tr>';
+	echo '<th width="150">Name</th>';
+	foreach( $levels as $l )
+	{
+		echo '<th width="50">' . $l['name'] . '</th>';
+	}
+echo '</tr>';
+$modules = $DB->MultiQuery('
+	SELECT *
+	FROM admin_module M
+	LEFT JOIN admin_level_module L ON M.id=L.module_id
+	WHERE active=1
+	ORDER BY `order`');
+foreach( $modules as $m )
+{
+	echo '<tr>';
+	echo '<td>' . $m['friendly_name'] . '</td>';
+	foreach( $levels as $l )
+	{
+		echo '<td align="center">' . ($l['level'] >= $m['level'] ? 'X' : '&nbsp;'). '</td>';
+	}
+	echo '</tr>';
+}
+echo '</table>';
+
+PrintFooter();
+
+?>
