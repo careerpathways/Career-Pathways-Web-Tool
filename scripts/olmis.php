b30a14b18d165b01010a30a55469b2c236f17be7
Aaron Parecki <aaron@parecki.com>
2009-05-06 21:45:44 +0000

    Adds script for exporting csv/xml to OLMIS database.
    git-svn-id: file:///Users/aaronpk/Sites/pathways-svn/trunk@507 e3ad4a73-d236-0410-80c3-982009a79c7a

---
+++ scripts/olmis.php
@@ -0,0 +1,82 @@
+#!/usr/bin/php
+<?php
+include('scriptinc.php');
+
+$drawings = $DB->MultiQuery('SELECT DISTINCT(parent_id) AS drawing_id, drawings.id AS version_id,
+		school_name, name AS drawing_name,
+		CONCAT("http://oregon.ctepathways.org/c/published/",code,".html") AS drawing_url,
+		CONCAT("http://",school_website) AS school_website
+	FROM objects 
+	JOIN drawings ON drawings.id = drawing_id 
+	JOIN drawing_main ON drawing_main.id=parent_id 
+	JOIN schools on school_id=schools.id 
+	WHERE content LIKE "%http://www.qualityinfo.org%"
+	AND published = 1
+	ORDER BY school_name');
+$olmis = array();
+$bycode = array();
+foreach( $drawings as $d )
+{
+	$soc = array();
+	$content = $DB->MultiQuery('SELECT content
+		FROM objects
+		WHERE drawing_id = ' . $d['version_id'] . '
+		AND content LIKE "%http://www.qualityinfo.org%"');
+	foreach( $content as $c )
+		// http://www.qualityinfo.org/olmisj/OIC?areacode=4101000000&rpttype=full&action=report&occ=292011&go=Continue
+		if( preg_match_all('|qualityinfo\.org/olmisj/OIC?.*?occ=([0-9]{6})|', $c['content'], $matches) )
+			foreach( $matches[1] as $m )
+				if( !in_array($m, $soc) )
+				{
+					$soc[] = $m;
+					$bycode[$m][] = $d;
+				}
+
+	$d['soc'] = $soc;
+	$olmis[] = $d;
+}
+
+
+// Generate CSV file
+$fp = fopen($SITE->cache_path('olmis').'olmis.csv', 'w');
+foreach( $olmis as $o )
+	foreach( $o['soc'] as $soc )
+		fwrite($fp, $o['school_name'].','.$soc.','.$o['drawing_url']."\n");
+fclose($fp);
+
+
+
+
+// Generate XML file
+ob_start();
+echo '<?xml version="1.0" encoding="UTF-8" ?>'."\n";
+?>		
+<cpolmisdata lastUpdated="<?=date('r')?>" lastUpdateTimestamp="<?=time()?>">
+<?php
+		foreach( $bycode as $soc=>$drawings ) {
+?>	<soc val="<?=$soc?>">
+<?php
+			foreach( $drawings as $drawing ) {
+?>		<drawing>
+			<url><?=$drawing['drawing_url']?></url>
+			<drawingname><?=htmlspecialchars($drawing['drawing_name'])?></drawingname>
+			<schoolname><?=$drawing['school_name']?></schoolname>
+			<schoolwebsite><?=$drawing['school_website']?></schoolwebsite>
+		</drawing>
+<?php
+			}
+?>
+	</soc>
+<?php
+		}
+?>
+</cpolmisdata>
+<?php		
+$xml = ob_get_clean();
+
+$fp = fopen($SITE->cache_path('olmis').'olmis.xml', 'w');
+fwrite($fp, $xml);
+fclose($fp);
+
+
+?>
