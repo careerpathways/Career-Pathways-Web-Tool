122c17f24379478c648d70a2440c04fdcf871747
Aaron Parecki <aaron@parecki.com>
2009-06-05 15:38:29 +0000

    admin cleanup script for categorizing drawings in bulk
    git-svn-id: file:///Users/aaronpk/Sites/pathways-svn/trunk@527 e3ad4a73-d236-0410-80c3-982009a79c7a

---
+++ www/a/cleanup.php
@@ -0,0 +1,228 @@
+<?php
+chdir("..");
+include("inc.php");
+require_once('Pager.php');
+
+if( Request('action') )
+{
+	switch(Request('action'))
+	{
+		case 'drawing_name':
+			$DB->Query('UPDATE drawing_main SET tagline = "'.mysql_real_escape_string(Request('name')).'" WHERE id = '.Request('drawing_id'));
+			$res = $DB->SingleQuery('SELECT tagline FROM drawing_main WHERE id = '.Request('drawing_id'));
+			echo '(' . json_encode(array('drawing_id'=>Request('drawing_id'), 'name'=>$res['tagline'])) . ')';
+			break;
+
+		case 'skillset':
+			$DB->Update('drawing_main', array(
+				'skillset_id'=>intval(Request('skillset_id')),
+				'program_id'=>0
+			), Request('drawing_id'));
+
+			if( Request('skillset_id') )
+				$programs_ = $DB->MultiQuery('SELECT * FROM programs WHERE skillset_id='.intval(Request('skillset_id')).' ORDER BY title');
+			else
+				$programs_ = $DB->MultiQuery('SELECT * FROM programs ORDER BY title');
+			
+			$programs = array(array('id'=>'0', 'title'=>''));
+			foreach( $programs_ as $p )
+			{
+				$programs[] = array('id'=>$p['id'], 'title'=>$p['title']);
+			}
+			
+			echo '('.json_encode($programs).')';
+			break;
+
+		case 'program':
+			if( Request('program_id') )
+			{
+				$skillset = $DB->SingleQuery('SELECT skillset_id FROM programs WHERE id='.intval(Request('program_id')));
+			}
+			else
+			{
+				$skillset = array('skillset_id'=>0);
+			}
+
+			$DB->Update('drawing_main', array(
+				'skillset_id'=>intval($skillset['skillset_id']),
+				'program_id'=>Request('program_id')
+			), Request('drawing_id'));
+		
+			echo '('.json_encode(array('skillset_id'=>$skillset['skillset_id'])).')';
+			break;
+			
+		default:
+			pa($_REQUEST);
+			break;
+	}
+	die();
+}	
+
+PrintHeader();
+?>
+<script type="text/javascript" src="/common/jquery-1.3.min.js"></script>
+<script type="text/javascript" src="/files/jquery.selectboxes.min.js"></script>
+<script type="text/javascript">
+	var $j = jQuery.noConflict();
+</script>
+<?php
+$total_drawings = $DB->SingleQuery('SELECT COUNT(*) AS num FROM drawing_main');
+
+$params = array(
+        'mode'          => 'Sliding',
+        'perPage'       => 30,
+        'delta'         => 2,
+        'totalItems'=> $total_drawings['num'],
+        'spacesBeforeSeparator' => 1,
+        'spacesAfterSeparator' => 1,
+        'urlVar' => 'pg',
+        'curPageSpanPre' => '',
+        'curPageSpanPost' => '',
+        'nextImg' => '&gt;',
+        'prevImg' => '&lt;',
+        'curPageLinkClassName' => 'active',
+        'clearIfVoid' => true,
+);
+$pager = &Pager::factory($params);
+$offset = $pager->getOffsetByPageId();
+
+$drawings = $DB->MultiQuery('
+	SELECT m.*, school_abbr
+	FROM drawing_main m
+	JOIN schools s ON m.school_id=s.id
+	ORDER BY tagline
+	LIMIT '.($offset[0]-1).', '.($params['perPage']).'
+');
+
+
+
+if( $pager->links != "" ) {
+        echo '<p><div class="pager_links">'.$pager->links.'</div></p>';
+} else {
+        echo '<br>';
+}
+
+echo '<table>' . "\n";
+
+foreach( $drawings as $d )
+{
+	echo '<tr>';
+		echo '<td><a href="/c/published/' . $d['id'] . '/view.htm" target="_blank">' . SilkIcon('magnifier.png') . '</td>';
+		echo '<td><a href="/a/drawings.php?action=drawing_info&id=' . $d['id'] . '" target="_blank">' . SilkIcon('cog.png') . '</td>';
+
+		echo '<td>' . $d['school_abbr'] . '</td>';
+
+		echo '<td><input type="text" class="drawing_name" id="drawing_'.$d['id'].'" value="' . htmlspecialchars($d['tagline']) . '" style="width: 220px" /></td>';
+
+		echo '<td><div id="skillset_'.$d['id'].'" class="skillset">';
+			echo GenerateSelectBoxDB('oregon_skillsets', 'skillset_id', 'id', 'title', 'title', $d['skillset_id'], array('0'=>''));
+		echo '</div></td>';
+
+		echo '<td><div id="program_'.$d['id'].'" class="program">';
+			if( intval($d['skillset_id']) != 0 )
+				$where = 'skillset_id = '.$d['skillset_id'];
+			else
+				$where = '';
+			echo GenerateSelectBoxDB('programs', 'program_id', 'id', 'title', 'title', $d['program_id'], array('0'=>''), $where);
+		echo '</div></td>';
+
+	echo '</tr>';
+}
+
+echo '</table>';
+?>
+<script type="text/javascript">
+$j(document).ready(function(){
+
+	$j(".drawing_name").change(function(){
+		var drawing_id = $j(this).attr("id").split("_")[1];
+		$j.get("cleanup.php",
+			{action: "drawing_name",
+			 drawing_id: $j(this).attr("id").split("_")[1],
+			 name: $j(this).val()},
+			 function(data){
+			 	json = eval(data);
+			 	if( json == null )
+			 	{
+				 	$j("#drawing_"+drawing_id).css({backgroundColor: "#FFAAAA"});
+				}
+				else
+				{
+				 	$j("#drawing_"+drawing_id).val(json.name);
+					blinkGreen("#drawing_"+drawing_id+" select");
+				}
+			 }
+		)
+	
+	});
+
+	$j('.skillset select').bind('change', function() {
+		var drawing_id = $j(this).parent().attr("id").split("_")[1];
+		$j(this).css({backgroundColor: '#FFFFAA'});
+		$j.post('cleanup.php',
+			{action: 'skillset',
+			 drawing_id: $j(this).parent().attr("id").split("_")[1],
+			 skillset_id: $j(this).val()
+			},
+			function(data) {
+				json = eval(data);
+				if( json == null )
+				{
+
+				}
+				else
+				{
+					programList = json;
+					loadProgramTitles(drawing_id);
+					blinkGreen("#skillset_"+drawing_id+" select");
+				}
+			}
+		);
+	});
+
+	$j('.program select').bind('change', function() {
+		var drawing_id = $j(this).parent().attr("id").split("_")[1];
+		$j(this).css({backgroundColor: '#FFFFAA'});
+		$j.post('cleanup.php',
+			{action: 'program',
+			 drawing_id: drawing_id,
+			 program_id: $j(this).val()
+			},
+			function(data) {
+				json = eval(data);
+				if( json == null )
+				{
+
+				}
+				else
+				{
+					$j('#skillset_'+drawing_id+' select').val(json.skillset_id);
+					blinkGreen("#program_"+drawing_id+" select");
+				}
+			}
+		);
+	});
+
+
+});
+
+function loadProgramTitles(id) {
+	$j("#program_"+id+" select").removeOption(/./);
+	for( var i=0; i<programList.length; i++ )
+	{
+		$j("#program_"+id+" select").addOption(programList[i].id, programList[i].title).val(0);
+	}
+}
+
+function blinkGreen(id)
+{
+ 	$j(id).css({backgroundColor: "#AAFFAA"});
+ 	setTimeout(function(){
+	 	$j(id).css({backgroundColor: "#FFFFFF"});
+ 	}, 300);
+}
+
+</script>
+<?php
+PrintFooter();
+?>
