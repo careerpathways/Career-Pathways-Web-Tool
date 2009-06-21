d6fa6abeea68b28c13291d208770f13f07ff1200
Aaron Parecki <aaron@parecki.com>
2009-06-21 20:34:38 +0000

    rewrote deleted olmis script
    git-svn-id: file:///Users/aaronpk/Sites/pathways-svn/trunk@542 e3ad4a73-d236-0410-80c3-982009a79c7a

--- scripts/olmis.php
+++ scripts/olmis.php
@@ -2,82 +2,28 @@
 <?php
 include('scriptinc.php');
 
-$drawings = $DB->MultiQuery('SELECT DISTINCT(parent_id) AS drawing_id, drawings.id AS version_id,
-		school_name, name AS drawing_name,
+$olmis = $DB->MultiQuery('
+	SELECT IF(name="",p.title,name) AS drawing_name,
+		drawing_main.id AS drawing_id, drawings.id AS version_id,
+		school_name,
 		CONCAT("http://oregon.ctepathways.org/c/published/",parent_id,"/view.html") AS drawing_url,
-		CONCAT("http://",school_website) AS school_website
-	FROM objects 
-	JOIN drawings ON drawings.id = drawing_id 
+		CONCAT("http://",school_website) AS school_website,
+		olmis_id
+	FROM drawings
 	JOIN drawing_main ON drawing_main.id=parent_id 
+	LEFT JOIN programs AS p ON drawing_main.program_id=p.id
+	JOIN olmis_links ON olmis_links.drawing_id=drawing_main.id
 	JOIN schools on school_id=schools.id 
-	WHERE content LIKE "%http://www.qualityinfo.org%"
 	AND published = 1
-	ORDER BY school_name');
-$olmis = array();
-$bycode = array();
-foreach( $drawings as $d )
-{
-	$soc = array();
-	$content = $DB->MultiQuery('SELECT content
-		FROM objects
-		WHERE drawing_id = ' . $d['version_id'] . '
-		AND content LIKE "%http://www.qualityinfo.org%"');
-	foreach( $content as $c )
-		// http://www.qualityinfo.org/olmisj/OIC?areacode=4101000000&rpttype=full&action=report&occ=292011&go=Continue
-		if( preg_match_all('|qualityinfo\.org/olmisj/OIC?.*?occ=([0-9]{6})|', $c['content'], $matches) )
-			foreach( $matches[1] as $m )
-				if( !in_array($m, $soc) )
-				{
-					$soc[] = $m;
-					$bycode[$m][] = $d;
-				}
-
-	$d['soc'] = $soc;
-	$olmis[] = $d;
-}
-
+	GROUP BY drawing_main.id, olmis_id
+	ORDER BY school_name, drawing_main.id');
 
 // Generate CSV file
 $fp = fopen($SITE->cache_path('olmis').'olmis.csv', 'w');
 foreach( $olmis as $o )
-	foreach( $o['soc'] as $soc )
-		fwrite($fp, $o['school_name'].','.$soc.','.$o['drawing_url'].',"'.trim($o['drawing_name']).'"'."\n");
+{
+	fwrite($fp, $o['school_name'].','.$o['olmis_id'].','.$o['drawing_url'].',"'.trim($o['drawing_name']).'"'."\n");
+}
 fclose($fp);
-
-
-
-
-// Generate XML file
-ob_start();
-echo '<?xml version="1.0" encoding="UTF-8" ?>'."\n";
-?>		
-<cpolmisdata lastUpdated="<?=date('r')?>" lastUpdateTimestamp="<?=time()?>">
-<?php
-		foreach( $bycode as $soc=>$drawings ) {
-?>	<soc val="<?=$soc?>">
-<?php
-			foreach( $drawings as $drawing ) {
-?>		<roadmap>
-			<roadmapurl><?=$drawing['drawing_url']?></roadmapurl>
-			<embeddedurl></embeddedurl>
-			<drawingname><?=htmlspecialchars($drawing['drawing_name'])?></drawingname>
-			<schoolname><?=$drawing['school_name']?></schoolname>
-			<schoolwebsite><?=$drawing['school_website']?></schoolwebsite>
-		</roadmap>
-<?php
-			}
-?>
-	</soc>
-<?php
-		}
-?>
-</cpolmisdata>
-<?php		
-$xml = ob_get_clean();
-
-$fp = fopen($SITE->cache_path('olmis').'olmis.xml', 'w');
-fwrite($fp, $xml);
-fclose($fp);
-
 
 ?>
