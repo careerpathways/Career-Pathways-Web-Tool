0ad0394b6bea9b6a1decb210361b39426c9f9bcc
Aaron Parecki <aaron@parecki.com>
2009-07-19 00:18:46 +0000

    adds OLMIS links to stats page
    git-svn-id: file:///Users/aaronpk/Sites/pathways-svn/trunk@551 e3ad4a73-d236-0410-80c3-982009a79c7a

--- www/a/stats.php
+++ www/a/stats.php
@@ -152,8 +152,18 @@ PrintHeader();
 		getLayer('total_post_added').innerHTML = data[4];
 	}
 </script>
+<div style="float:right; width:220px; border: 1px #999 solid; font-size: 1.2em; line-height: 1.6em;">
+	<ul style="margin:0; padding: 10px; list-style-type: none;">
+		<li><a href="#user_stats">User Stats</a></li>
+		<li><a href="#drawing_counts">Drawing Counts</a></li>
+		<li><a href="#editing_history">Editing History</a></li>
+		<li><a href="#olmis_links">OLMIS Links</a></li>
+		<li><a href="#traffic_logs">Traffic Logs</a></li>
+	</ul>
+</div>
 <?php
 
+echo '<a name="user_stats"></a>';
 echo '<h2>User Stats</h2>';
 echo '<br>';
 
@@ -216,6 +226,7 @@ echo '<br>';
 
 // SNAPSHOT
 echo '<hr>';
+echo '<a name="drawing_counts"></a>';
 echo '<h2>Drawing Counts</h2>';
 echo '<br>';
 
@@ -288,6 +299,7 @@ echo '</tr></table>';
 
 // HISTORY
 echo '<hr>';
+echo '<a name="editing_history"></a>';
 echo '<h2>Editing History</h2>';
 
 foreach( $tables as $t )
@@ -423,9 +435,41 @@ foreach( $files as $file ) {
 
 
 
+// OLMIS LINKS
+echo '<hr>';
+echo '<a name="olmis_links"></a>';
+echo '<h2>OLMIS Links</h2>';
+
+$olmis = $DB->MultiQuery('
+	SELECT c.olmis_id, c.job_title
+	FROM olmis_links AS l
+	JOIN olmis_codes AS c ON l.olmis_id=c.olmis_id
+	GROUP BY l.olmis_id
+	ORDER BY job_title');
+
+echo count($olmis) . ' OLMIS reports currently link to published roadmaps';
+
+foreach( $olmis as $o ) {
+	echo '<div class="olmis_title"><a href="http://www.qualityinfo.org/olmisj/OIC?areacode=4101000000&rpttype=full&action=report&occ='.$o['olmis_id'].'&go=Continue#section11" target="_blank">' . $o['job_title'] . '</a></div>';
+	$drawings = $DB->MultiQuery('
+		SELECT IFNULL(p.title, d.name) AS name, d.id, schools.school_abbr
+		FROM olmis_links AS l
+		JOIN drawing_main AS d ON l.drawing_id=d.id
+		LEFT JOIN programs AS p ON d.program_id=p.id
+		JOIN schools ON d.school_id=schools.id
+		WHERE l.olmis_id = '.$o['olmis_id']);
+	foreach( $drawings as $d ) {
+		echo '<div class="olmis_roadmap"><a href="/c/published/' . $d['id'] . '/view.html" target="_blank">' . $d['school_abbr'] . ': ' . $d['name'] . '</a></div>';
+	}
+}
+
+
+
+
 // TRAFFIC LOGS
 
 echo '<hr>';
+echo '<a name="traffic_logs"></a>';
 echo '<h2>Traffic Logs</h2>';
 
 
