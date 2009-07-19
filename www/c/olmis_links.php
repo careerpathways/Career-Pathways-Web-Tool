4eb11e9025563ccd1fa78bf8b179f430b8900ea6
Aaron Parecki <aaron@parecki.com>
2009-07-19 19:20:13 +0000

    adding publicly accessible view of olmis links
    git-svn-id: file:///Users/aaronpk/Sites/pathways-svn/trunk@553 e3ad4a73-d236-0410-80c3-982009a79c7a

---
+++ www/c/olmis_links.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+	<title>OLMIS Links</title>
+	<style type="text/css">
+		body {
+			font-family: verdana, sans-serif;
+			font-size: 10px;
+		}
+		.olmis_title {
+			margin-top: 8px;
+			font-size: 1.2em;
+		}
+		.olmis_roadmap {
+			margin-left: 30px;
+		}
+	</style>
+</head>
+<body>
+<?php
+chdir("..");
+include("inc.php");
+
+$olmis = $DB->MultiQuery('
+	SELECT c.olmis_id, c.job_title
+	FROM olmis_links AS l
+	JOIN olmis_codes AS c ON l.olmis_id=c.olmis_id
+	GROUP BY l.olmis_id
+	ORDER BY job_title');
+
+echo count($olmis) . ' OLMIS reports currently link to published roadmaps';
+
+foreach( $olmis as $o ) {
+	echo '<div class="olmis_title"><a href="http://www.qualityinfo.org/olmisj/OIC?areacode=4101000000&rpttype=full&action=report&occ='.$o['olmis_id'].'&go=Continue#section11" target="_blank">' . $o['job_title'] . '</a></div>';
+	$drawings = $DB->MultiQuery('
+		SELECT IFNULL(p.title, d.name) AS name, d.id, schools.school_abbr
+		FROM olmis_links AS l
+		JOIN drawing_main AS d ON l.drawing_id=d.id
+		LEFT JOIN programs AS p ON d.program_id=p.id
+		JOIN schools ON d.school_id=schools.id
+		WHERE l.olmis_id = '.$o['olmis_id']);
+	foreach( $drawings as $d ) {
+		echo '<div class="olmis_roadmap"><a href="/c/published/' . $d['id'] . '/view.html" target="_blank">' . $d['school_abbr'] . ': ' . $d['name'] . '</a></div>';
+	}
+}
+
+?>
+</body>
+</html>
