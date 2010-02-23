59e4f024da2dc1690491cc4adaf79aacb067265d
Aaron Parecki <aaron@parecki.com>
2010-02-23 20:42:32 +0000

    Roadmap embedding now writes absolute URLs so schools can embed drawings with proxy scripts easier.
    git-svn-id: file:///Users/aaronpk/Sites/pathways-svn/trunk@577 e3ad4a73-d236-0410-80c3-982009a79c7a

--- www/c/view/chart_include.php
+++ www/c/view/chart_include.php
@@ -3,6 +3,6 @@
 <?php require('chart_data_js.php'); ?>
 </script>
 
-<!--[if IE]><script type="text/javascript" src="/files/excanvas.js"></script><![endif]--> 
-<script type="text/javascript" src="/files/prototype.js"></script>
-<script type="text/javascript" src="/c/chview.js"></script>
+<!--[if IE]><script type="text/javascript" src="http://<?= $_SERVER['SERVER_NAME'] ?>/files/excanvas.js"></script><![endif]--> 
+<script type="text/javascript" src="http://<?= $_SERVER['SERVER_NAME'] ?>/files/prototype.js"></script>
+<script type="text/javascript" src="http://<?= $_SERVER['SERVER_NAME'] ?>/c/chview.js"></script>
