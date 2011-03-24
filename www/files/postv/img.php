564237d9de43d653d2037ca8d0bc2a3ec60ffca8
Aaron Parecki <aaron@parecki.com>
2011-03-24 13:05:56 -0700

    Rewrite image generation scripts to output text using built-in gd fonts if ttf library is not availbale.

--- www/files/postv/img.php
+++ www/files/postv/img.php
@@ -13,16 +13,23 @@ $filename = $SITE->cache_path('post_vertical') . $hash;
 
 header("Content-type: image/png");
 
-if( !file_exists($filename) ) {
-	$bbox = imagettfbbox($font_size, 90, $title_font, $text);
-	$dst = imagecreatetruecolor(abs($bbox[6])-2, abs($bbox[3]) + 8);
+if(1 || !file_exists($filename) ) {
+	if(function_exists('imagettftext')) {
+		$bbox = imagettfbbox($font_size, 90, $title_font, $text);
+		$dst = imagecreatetruecolor(abs($bbox[6])-2, abs($bbox[3]) + 8);
+	} else {
+		$dst = imagecreatetruecolor(12, 300);
+	}
 
 	$c_bkg = imagecolorallocatestr($dst, "FFFFFF");
 	$c_title = imagecolorallocatestr($dst, "000000");
 
 	imagefill($dst, 0,0, $c_bkg);
 
-	imagettftext($dst, $font_size, 90, abs($bbox[6])-2,abs($bbox[3])+6, $c_title, $title_font, $text);
+	if(function_exists('imagettftext'))
+		imagettftext($dst, $font_size, 90, abs($bbox[6])-2,abs($bbox[3])+6, $c_title, $title_font, $text);
+	else
+		imagestringup($dst, 1, 2,290, $text, $c_title);
 
 	imagepng($dst, $filename);
 	imagepng($dst);
@@ -30,4 +37,3 @@ if( !file_exists($filename) ) {
 	readfile($filename);
 }
 
-?>
--- www/files/titles/title.php
+++ www/files/titles/title.php
@@ -22,11 +22,25 @@ if( $i=strpos($_SERVER['REQUEST_URI'],'?') ) {
 if( $school == '-' ) $school = '';
 
 $hash = md5($school.$title.Request('type'));
-$filename = $SITE->cache_path("titles")."/".$hash;
+$filename = $SITE->cache_path("titles").$hash;
 
 $width = 800;
 
 header("Content-type: image/png");
+
+/*
+if(!function_exists('imagettftext')) {
+	$im = imagecreatetruecolor($width, 19);
+	$trans = imagecolorallocatealpha($im, 255, 255, 255, 127);
+	$white = imagecolorallocate($im, 255, 255, 255);
+	imagefill($im, 0,0, $trans);
+	imagestring($im, 3, 10, 2, 'imagettftext function not found, probably your PHP does not have freetype support enabled.', $white);
+	imagesavealpha($im, TRUE);
+	imagepng($im);
+	die();
+}
+*/
+
 
 if( !file_exists($filename) ) {
 
@@ -41,21 +55,30 @@ if( !file_exists($filename) ) {
 
 	imagefilledrectangle($dst, 0,0, $width,imagesy($dst), $c_cpbkg);
 
-	$bbox = imagettftext($dst, $font_size, 0, 4,16, (Request('type')=='pathways'?$c_cptitle2:$c_cptitle1), $font_thick, $school);
-	if( Request('type') == 'pathways' )
-	{
-		$bbox = imagettftext($dst, $font_size-1, 0, $bbox[2]+2,16, $c_cptitle1, $font_thin, l('drawing head pathways 1'));
-		$bbox = imagettftext($dst, $font_size, 0, $bbox[2]+2,16, $c_cptitle2, $font_thick, l('drawing head pathways 2'));
-	}
-	else
-	{
-		$bbox = imagettftext($dst, $font_size-1, 0, $bbox[2]+2,16, $c_cptitle2, $font_thin, l('drawing head post 1'));
-		$bbox = imagettftext($dst, $font_size, 0, $bbox[2]+3,16, $c_cptitle2, $font_thick, l('drawing head post 2'));
-	}
+	if(function_exists('imagettftext')) {
+	
+		$bbox = imagettftext($dst, $font_size, 0, 4,16, (Request('type')=='pathways'?$c_cptitle2:$c_cptitle1), $font_thick, $school);
+		if( Request('type') == 'pathways' )
+		{
+			$bbox = imagettftext($dst, $font_size-1, 0, $bbox[2]+2,16, $c_cptitle1, $font_thin, l('drawing head pathways 1'));
+			$bbox = imagettftext($dst, $font_size, 0, $bbox[2]+2,16, $c_cptitle2, $font_thick, l('drawing head pathways 2'));
+		}
+		else
+		{
+			$bbox = imagettftext($dst, $font_size-1, 0, $bbox[2]+2,16, $c_cptitle2, $font_thin, l('drawing head post 1'));
+			$bbox = imagettftext($dst, $font_size, 0, $bbox[2]+3,16, $c_cptitle2, $font_thick, l('drawing head post 2'));
+		}
 
-	imagefilledrectangle($dst, $bbox[2]+5,0, $width,imagesy($dst), $c_bkg);
-	imagettftext($dst, 12, 0, $bbox[2]+10,15, $c_title, $title_font, $title);
+		imagefilledrectangle($dst, $bbox[2]+5,0, $width,imagesy($dst), $c_bkg);
+		imagettftext($dst, 12, 0, $bbox[2]+10,15, $c_title, $title_font, $title);
+	} else {
+		$white = imagecolorallocate($dst, 255,255,255);
+	
+		imagefilledrectangle($dst, 0,0, $width,imagesy($dst), $c_bkg);
 
+		imagestring($dst, 3, 4,3, $school . ": " . $title, $white);
+	}
+	
 	imagepng($dst, $filename);
 	imagepng($dst);
 } else {
