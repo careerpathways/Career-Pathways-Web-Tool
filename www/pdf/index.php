936c77acbc2401933e165b28bbbdbd7f54fba574
Aaron Parecki <aaron@parecki.com>
2011-03-24 14:07:42 -0700

    Fix for #69 - better PDF filenames including school name and version number

--- www/pdf/index.php
+++ www/pdf/index.php
@@ -8,44 +8,50 @@ switch(request('mode'))
 		$url = 'http://' . $_SERVER['SERVER_NAME'] . '/c/published/' . request('drawing_id') . '/view.html';
 		$filename = 'published-' . request('drawing_id') . '.pdf';
 		$drawing = $DB->SingleQuery('SELECT drawing_main.*,
-				IF(drawing_main.name="", p.title, drawing_main.name) AS full_name
+				IF(drawing_main.name="", p.title, drawing_main.name) AS full_name, s.school_name
 			FROM drawing_main
 			LEFT JOIN programs AS p ON drawing_main.program_id=p.id
+			LEFT JOIN schools AS s ON s.id = drawing_main.school_id
 			WHERE drawing_main.id = ' . request('drawing_id'));
-		$name = $drawing['full_name'] . '.pdf';
+		$version = $DB->SingleQuery('SELECT version_num FROM drawings WHERE parent_id = ' . $drawing['id'] . ' AND published = 1');
+		$name = $drawing['school_name'] . ' - ' . $drawing['full_name'] . ' - Version ' . $version['version_num'];
 		break;
 
 	case 'version':
 		$url = 'http://' . $_SERVER['SERVER_NAME'] . '/c/version/' . request('drawing_id') . '/' . request('version_id') . '.html';
 		$filename = 'version-' . request('version_id') . '.pdf';
 		$drawing = GetDrawingInfo(request('version_id'));
-		$name = $drawing['full_name'] . ' - Version ' . $drawing['version_num'] . '.pdf';
+		$name = $drawing['school_name'] . ' - ' . $drawing['full_name'] . ' - Version ' . $drawing['version_num'];
 		break;
 		
 	case 'post_drawing':
 		$url = 'http://' . $_SERVER['SERVER_NAME'] . '/c/post/' . request('drawing_id') . '/view.html?hidecoursedescription';
 		$filename = 'post-published-' . request('drawing_id') . '.pdf';
 		$drawing = $DB->SingleQuery('SELECT post_drawing_main.*,
-				IF(post_drawing_main.name="", p.title, post_drawing_main.name) AS full_name
+				IF(post_drawing_main.name="", p.title, post_drawing_main.name) AS full_name, s.school_name
 			FROM post_drawing_main
 			LEFT JOIN programs AS p ON post_drawing_main.program_id=p.id
+			LEFT JOIN schools AS s ON s.id = post_drawing_main.school_id
 			WHERE post_drawing_main.id = ' . request('drawing_id'));
-		$name = $drawing['full_name'] . '.pdf';
+		$version = $DB->SingleQuery('SELECT version_num FROM post_drawings WHERE parent_id = ' . $drawing['id'] . ' AND published = 1');
+		$name = $drawing['school_name'] . ' - ' . $drawing['full_name'] . ' - Version ' . $version['version_num'];
 		break;
 		
 	case 'post_version':
 		$url = 'http://' . $_SERVER['SERVER_NAME'] . '/c/post/' . request('drawing_id') . '/' . request('version_id') . '.html?hidecoursedescription';
 		$filename = 'post-version-' . request('version_id') . '.pdf';
 		$drawing = GetDrawingInfo(request('version_id'), 'post');
-		$name = $drawing['name'] . ' - Version ' . $drawing['version_num'] . '.pdf';
+		$name = $drawing['school_name'] . ' - ' . $drawing['name'] . ' - Version ' . $drawing['version_num'];
 		break;		
 		
 	case 'post_view':
 		$url = 'http://' . $_SERVER['SERVER_NAME'] . '/c/study/' . request('id') . '/view.html?print&hidecoursedescription';
 		$filename = 'post-view-' . request('id') . '.pdf';
-		$drawing = $DB->SingleQuery('SELECT name FROM vpost_views
-			WHERE id = ' . request('id'));
-		$name = $drawing['name'] . '.pdf';
+		$drawing = $DB->SingleQuery('SELECT name, school_name
+			FROM vpost_views v
+			JOIN schools s on v.school_id = s.id
+			WHERE v.id = ' . request('id'));
+		$name = $drawing['school_name'] . ' - ' . $drawing['name'];
 		break;
 		
 	default:
@@ -56,6 +62,8 @@ $fullPath = 'pdf/tmp/' . $filename;
 
 if(!file_exists($fullPath) || filemtime($fullPath))
 	shell_exec('/usr/bin/wkhtmltopdf-i386 "' . $url . '" ' . $fullPath);
+
+$name = preg_replace('/[^A-Za-z0-9 -]/', '', $name) . '.pdf';
 
 header('Content-type: application/pdf');
 header('Content-disposition: attachment; filename="' . addslashes($name) . '"');
