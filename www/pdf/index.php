2e2ce56c2600fde7b3696cab8b9af339c76fe06f
Aaron Parecki <aaron@parecki.com>
2011-05-01 13:49:08 -0700

    Fixes PDF download for IE

--- www/pdf/index.php
+++ www/pdf/index.php
@@ -68,5 +68,11 @@ $name = preg_replace('/[^A-Za-z0-9 -]/', '', $amp_repl) . '.pdf';
 
 header('Content-type: application/pdf');
 header('Content-disposition: attachment; filename="' . addslashes($name) . '"');
+header('Cache-control: no-cache');
+if( function_exists('header_remove') ) {
+	header_remove('Pragma');
+} else {
+	header('Pragma:');
+}
 readfile($fullPath);
 
