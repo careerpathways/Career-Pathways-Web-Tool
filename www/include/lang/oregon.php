78f8dda2f9468fd9ccb01cb999d26ab0d548b5c4
Michael Calabrese <michael.calabrese@lunarlogic.com>
2014-04-23 11:31:54 -0700

    Plan of study is now Programm of study

--- www/include/lang/oregon.php
+++ www/include/lang/oregon.php
@@ -3,7 +3,7 @@ class Localize_Strings extends Localize
 {
 	public function __construct()
 	{
-		$this->add('drawing head post 1', 'PLAN');
+		$this->add('drawing head post 1', 'PROGRAM');
 		$this->add('drawing head post 2', 'OF STUDY');
 		$this->add('drawing head pathways 1', 'CAREER');
 		$this->add('drawing head pathways 2', 'PATHWAYS');
