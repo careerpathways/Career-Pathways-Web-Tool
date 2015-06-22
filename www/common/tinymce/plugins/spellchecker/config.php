ebd9a6eb41122fea0b00b37894c21449755dc2c9
Aaron Martins <me@aaronmartins.com>
2015-06-22 15:08:20 -0700

    Upgrades tinymce to 3.5.11 and tinymce spellchecker to 2.0.6.1

--- www/common/tinymce/plugins/spellchecker/config.php
+++ www/common/tinymce/plugins/spellchecker/config.php
@@ -5,8 +5,8 @@
  * @package MCManager.includes
  */
 	// General settings
-	//$config['general.engine'] = 'GoogleSpell';
-	$config['general.engine'] = 'PSpell';
+	$config['general.engine'] = 'GoogleSpell';
+	//$config['general.engine'] = 'PSpell';
 	//$config['general.engine'] = 'PSpellShell';
 	//$config['general.remote_rpc_url'] = 'http://some.other.site/some/url/rpc.php';
 
