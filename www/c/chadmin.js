ec4635fcdbfcdc0d22784719a5cc54d3ea16558d
Aaron Martins <me@aaronmartins.com>
2015-08-28 14:00:27 -0700

    Fixes Circles edit menu [LL WA t28]

--- www/c/chadmin.js
+++ www/c/chadmin.js
@@ -1476,7 +1476,7 @@ chColor.each(function(color) {
 	});
 });
 boxColorBackgroundMenu.addItem({
-	text: '<span title="Transparent" style="border:1px solid grey;color:red;padding: 0 3px;" title="My tip">&#216;</span>',
+	text: '<span title="Transparent" style="border:1px solid grey;color:red;padding: 0 3px;" title="Transparent">&#216;</span>',
 	onclick: {fn: onColorBackgroundSelect, obj: 'transparent', scope: boxColorBackgroundMenu}
 });
 var boxTitleColorMenu = new YAHOO.widget.Menu('boxTitleColorMenu');
@@ -1493,13 +1493,11 @@ var linkBoxesMenuItem = new YAHOO.widget.MenuItem(LINK_TO_LABEL, {onclick: {fn: 
 // create the box context menu
 ChartBox.contextMenu = new YAHOO.widget.ContextMenu('ChartBox.contextMenu');
 ChartBox.contextMenu.addItems([[
-	// {text: 'Edit', submenu: editBoxMenu},
 	{text: 'Edit Content', onclick: {fn: onEditContentSelect}},
 	{text: 'Edit Title', onclick: {fn: onEditTitleSelect}},
-	{text: 'Edit Title Color', submenu: boxTitleColorMenu},
-	{text: 'Color', submenu: boxColorMenu},
+	{text: 'Title Color', submenu: boxTitleColorMenu},
+	{text: 'Border Color', submenu: boxColorMenu},
 	{text: 'Background Color', submenu: boxColorBackgroundMenu},
-	// {text: 'Box Type', submenu: typeMenu},
 	linkBoxesMenuItem,
 	{text: 'Duplicate', onclick: {fn: onDuplicateSelect}}
 ],
@@ -1513,16 +1511,20 @@ ChartBox.contextMenu.addItems([[
 var circleColorMenu = new YAHOO.widget.Menu('circleColorMenu');
 chColor.each(function(color) {
 	circleColorMenu.addItem({
-		text: '<span style="background-color: #' + color + '">&nbsp;&nbsp;&nbsp;&nbsp;</span>',
+		text: '<span style="border:1px solid grey;background-color: #' + color + '">&nbsp;&nbsp;&nbsp;&nbsp;</span>',
 		onclick: {fn: onColorSelect, obj: color, scope: circleColorMenu}
 	});
 });
 var circleColorBackgroundMenu = new YAHOO.widget.Menu('circleColorBackgroundMenu');
 chColor.each(function(color) {
 	circleColorBackgroundMenu.addItem({
-		text: '<span style="background-color: #' + color + '">&nbsp;&nbsp;&nbsp;&nbsp;</span>',
+		text: '<span style="border:1px solid grey;background-color: #' + color + '">&nbsp;&nbsp;&nbsp;&nbsp;</span>',
 		onclick: {fn: onColorBackgroundSelect, obj: color, scope: circleColorBackgroundMenu}
 	});
+});
+circleColorBackgroundMenu.addItem({
+	text: '<span title="Transparent" style="border:1px solid grey;color:red;padding: 0 3px;" title="Transparent">&#216;</span>',
+	onclick: {fn: onColorBackgroundSelect, obj: 'transparent', scope: circleColorBackgroundMenu}
 });
 var circleTitleColorMenu = new YAHOO.widget.Menu('circleTitleColorMenu');
 var titleColors = ['ffffff', '000000'];
@@ -1538,13 +1540,9 @@ var linkCirclesMenuItem = new YAHOO.widget.MenuItem(LINK_TO_LABEL, {onclick: {fn
 // create the box context menu
 ChartCircle.contextMenu = new YAHOO.widget.ContextMenu('ChartCircle.contextMenu');
 ChartCircle.contextMenu.addItems([[
-	// {text: 'Edit', submenu: editBoxMenu},
 	{text: 'Edit Content', onclick: {fn: onEditContentSelect}},
-	{text: 'Edit Title', onclick: {fn: onEditTitleSelect}},
-	{text: 'Edit Title Color', submenu: circleTitleColorMenu},
-	{text: 'Color', submenu: circleColorMenu},
+	{text: 'Border Color', submenu: circleColorMenu},
 	{text: 'Background Color', submenu: circleColorBackgroundMenu},
-	// {text: 'Box Type', submenu: typeMenu},
 	linkCirclesMenuItem,
 	{text: 'Duplicate', onclick: {fn: onDuplicateSelect}}
 ],
