790ae54af098c72367529f060b57829f343fb7a4
Aaron Martins <me@aaronmartins.com>
2015-09-30 11:11:40 -0700

    Minor fix for PDF shrinking to upper-left corner, now doesn't enforce container resize on drafts. [LL WA t41]

--- www/c/chview.js
+++ www/c/chview.js
@@ -12,8 +12,10 @@ Charts = {
     		var points = Charts.components.invoke('getShape').invoke('getBounds').pluck('bottomRight');
     		Charts.bounds = new Geometry.Bounds(points);
     		var bottomRight = Charts.bounds.bottomRight;
-    		jQuery('#'+canvas_container).width(bottomRight.x);
-    		jQuery('#'+canvas_container).height(bottomRight.y);
+    		if(Charts.drawingStatus !== 'draft') {
+	    		jQuery('#'+canvas_container).width(bottomRight.x);
+	    		jQuery('#'+canvas_container).height(bottomRight.y);
+    		}
 		};
     }
     else {
